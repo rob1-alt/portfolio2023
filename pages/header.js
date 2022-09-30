21c37c14028617fb87c406cf7d212910766fdd0f
rob1-alt <robin.pautigny@hetic.net>
2022-09-30 10:19:46 +0200

    🔥 Adding files, removes others useless files 🔥

--- pages/header.js
+++ pages/header.js
@@ -1,21 +1,16 @@
 import styles from '../styles/Home.module.css'
 import Image from 'next/image'
+import Logo from '../src/img/logo.png'
 
 
 export default function header(){
     return(
         <div className={styles.navbar}>
+            <Image className={styles.logoNav} src={Logo} alt="/"/>   
             <div className={styles.name}>
-            <Image
-                src="/../src/img/logo.png"
-                sizes="(max-width: 768px) 100vw,
-              (max-width: 1200px) 50vw,
-              33vw"
-              layout='fill'
-            
-            />
-                <p >Robin Pautigny</p>
-            </div>                
+                <p>Robin Pautigny</p>
+            </div>   
+                    
             <ul className={styles.menuNav}>
                     <li>About</li>
                     <li>Work</li>
